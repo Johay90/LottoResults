d51524fc1fe0daf18da722f9c8c317f69e240f12
JONATHAN MUMMERY <johay.jjj@googlemail.com>
2024-03-11 17:43:24 +0000

    LottoDraw to own file

---
+++ LottoResults/LottoDraw.cs
@@ -0,0 +1,16 @@
+﻿public class LottoDraw
+{
+    public string DrawDate { get; set; }
+    public List<int> Results { get; set; } = new List<int>();
+    public int Jackpot { get; set; }
+    public string Status { get; set; }
+
+    public override string ToString()
+    {
+        string resultsString = string.Join(", ", Results);
+        string formattedJackpot = Jackpot.ToString("N0"); 
+        string rollover = Status.Length > 0 ? Status : "No Rollover";
+
+        return $"{DrawDate} - Results: {resultsString} - Jackpot: £{formattedJackpot} - {rollover}";
+    }
+}
--- LottoResults/Program.cs
+++ LottoResults/Program.cs
@@ -25,21 +25,3 @@ do
 
 Console.WriteLine("Application will now exit on keypress.");
 Console.ReadKey();
-
-
-public class LottoDraw
-{
-    public string DrawDate { get; set; }
-    public List<int> Results { get; set; } = new List<int>();
-    public int Jackpot { get; set; }
-    public string Status { get; set; }
-
-    public override string ToString()
-    {
-        string resultsString = string.Join(", ", Results);
-        string formattedJackpot = Jackpot.ToString("N0"); 
-        string rollover = Status.Length > 0 ? Status : "No Rollover";
-
-        return $"{DrawDate} - Results: {resultsString} - Jackpot: £{formattedJackpot} - {rollover}";
-    }
-}
