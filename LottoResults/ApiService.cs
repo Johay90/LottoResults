b3c5e3f42c83553c7f5d90ecf66f7d6f22399faa
JONATHAN MUMMERY <johay.jjj@googlemail.com>
2024-03-11 17:39:15 +0000

    Pick an entry to output lotto results

---
+++ LottoResults/ApiService.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+public class ApiService
+{
+    public async Task<List<LottoDraw>> GetAllResults()
+    {
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync("https://www.lottery.co.uk/lotto/results/past");
+        response.EnsureSuccessStatusCode();
+        string htmlContent = await response.Content.ReadAsStringAsync();
+
+        var htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(htmlContent);
+
+        var table = htmlDoc.DocumentNode.SelectSingleNode("//table[@class='table lotto mobFormat']");
+        var lottoDraws = new List<LottoDraw>();
+
+        foreach (var row in table.SelectNodes(".//tr").Skip(1))
+        {
+            var cells = row.SelectNodes(".//td");
+            if (cells != null && cells.Count >= 3)
+            {
+                var jackpotText = cells[2].SelectSingleNode(".//strong").InnerText.Trim();
+                var cleanedJackpot = Regex.Replace(jackpotText, @"[^\d]", "");
+
+                var draw = new LottoDraw
+                {
+                    DrawDate = cells[0].InnerText.Trim(),
+                    Results = cells[1].SelectNodes(".//div[@class='result small lotto-ball' or @class='result small lotto-bonus-ball']")
+                        .Select(node => int.Parse(node.InnerText.Trim()))
+                        .ToList(),
+                    Jackpot = int.Parse(cleanedJackpot),
+                    Status = cells[2].SelectNodes(".//strong").Count() > 1 ? cells[2].SelectNodes(".//strong")[1].InnerText.Trim() : string.Empty
+                };
+
+                lottoDraws.Add(draw);
+            }
+        }
+        return lottoDraws;
+    }
+}
--- LottoResults/Program.cs
+++ LottoResults/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var api = new ApiService();
+var results = await api.GetAllResults();
+Console.WriteLine("We have results from the following dates:");
+
+for (int i = 0; i < results.Count; i++)
+{
+    Console.WriteLine($"{i + 1}. {results[i].DrawDate}");
+}
+
+do
+{
+    Console.WriteLine("Pick a lotto reuslt by typing it's ID.");
+    string input = Console.ReadLine();
+    if (int.TryParse(input, out int res))
+    {
+        res -= 1;
+        if (results.ElementAtOrDefault(res) != null)
+        {
+            Console.WriteLine(results[res]);
+            break;
+        }
+    }
+} while (true);
+
+
+Console.WriteLine("Application will now exit on keypress.");
+Console.ReadKey();
+
+
+public class LottoDraw
+{
+    public string DrawDate { get; set; }
+    public List<int> Results { get; set; } = new List<int>();
+    public int Jackpot { get; set; }
+    public string Status { get; set; }
+
+    public override string ToString()
+    {
+        string resultsString = string.Join(", ", Results);
+        string formattedJackpot = Jackpot.ToString("N0"); 
+        string rollover = Status.Length > 0 ? Status : "No Rollover";
+
+        return $"{DrawDate} - Results: {resultsString} - Jackpot: £{formattedJackpot} - {rollover}";
+    }
+}
